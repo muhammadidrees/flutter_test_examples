a9a0fd264ef95c542d7ec42b4a0f580c6cd4ed80
Muhammad Idrees <midrees097@gmail.com>
2021-10-19 13:55:25 +0500

    refactored test to avoid repetation

--- test/home_screen_test.dart
+++ test/home_screen_test.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_test_examples/home_screen.dart';
 
+Future<void> pumpHomeScreenWidget(WidgetTester tester) async =>
+    tester.pumpWidget(
+      const MaterialApp(
+        home: HomeScreen(),
+      ),
+    );
+
 void main() {
   group("HomeScreen", () {
     testWidgets("renders home screen", (tester) async {
-      await tester.pumpWidget(
-        const MaterialApp(
-          home: HomeScreen(),
-        ),
-      );
-
+      await pumpHomeScreenWidget(tester);
       expect(find.byType(HomeScreen), findsOneWidget);
     });
 
