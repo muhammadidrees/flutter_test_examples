3a6d1e020afadbef2af04035632bb7e8128c89c0
Muhammad Idrees <midrees097@gmail.com>
2021-10-19 14:12:17 +0500

    refactored test to avoid repetation

--- test/navigation/screen_navigation_test.dart
+++ test/navigation/screen_navigation_test.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_test_examples/navigation/navigation.dart';
 
+Future<void> pumpScreenNavigationButtonWidget(WidgetTester tester) async =>
+    tester.pumpWidget(
+      const Scaffold(
+        body: ScreenNavigationButton(),
+      ),
+    );
+
 void main() {
   group("Screen Navigation", () {
     testWidgets("can render", (tester) async {
-      await tester.pumpWidget(const MaterialApp(
-        home: Scaffold(
-          body: ScreenNavigationButton(),
-        ),
-      ));
+      await pumpScreenNavigationButtonWidget(tester);
 
       expect(find.byType(ScreenNavigationButton), findsOneWidget);
     });
